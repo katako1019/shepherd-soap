0a57e6d14884f964c5db0e1ec434cae1f6b6c19a
katako1019 <58834287+katako1019@users.noreply.github.com>
2023-03-28 15:18:27 +0800

    first commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: '作品' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
